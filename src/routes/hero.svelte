f40edb0336f5af5f53ed24a240a47a98fdbeccf7
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2024-02-20 21:22:15 +0000

    feature: mobile site up

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -1,22 +1,22 @@
-<div class="flex w-full h-full flex-row" id="hero">
-    <div class="py-24 pl-24 flex w-[60%] h-full flex-col justify-between">
-        <div class="flex flex-col gap-8 w-3/4">
-            <span class="text-4xl">Inayat Shergill</span>
-            <span class="text-xl">Doctor, Graduated from DMC Ludhiana</span>
-            <span class="text-xl pr-12"
+<div class="flex w-full h-full md:flex-row flex-col-reverse" id="hero">
+    <div class="py-4 md:py-24 md:pl-24 flex w-full md:w-[60%] h-[40%] md:h-full flex-col justify-between items-center md:items-start gap-4 md:gap-8">
+        <div class="flex flex-col gap-1 sm:gap-8 w-full md:w-3/4 text-center md:text-start px-8 md:px-0">
+            <span class="text-3xl md:text-4xl">Inayat Shergill</span>
+            <span class="text-base md:text-xl mt-4 md:mt-0 text-justify md:text-start">Doctor, Graduated from DMC Ludhiana</span>
+            <span class="text-base md:text-xl pr-0 md:pr-12 text-justify md:text-start"
                 >As a highly dedicated, detail oriented and empathetic
                 individual, I am eager to make a positive impact on patient
                 lives. Through hard work and willingness to learn, I am
                 committed to make a significant contribution to patient care.</span
             >
         </div>
-        <div class="flex gap-12 text-xl items-center">
+        <div class="flex gap-12 text-xl items-center mb-8">
             <a href="">linkedin</a>
             <a href="">email</a>
             <a class="bg-black text-white py-2 px-4" href=""> contact </a>
         </div>
     </div>
-    <div class="flex w-[40%] h-full">
+    <div class="flex w-full md:w-[40%] h-[60%] md:h-full">
         <img src="img-2.png" alt="" class="w-full h-full object-cover" />
     </div>
 </div>
